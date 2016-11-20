e93b535e6537fced82d98e65c516c3183256db33
Jules Martin <email": null>
2016-11-19 19:41:00 -0800

    main page displaying all the categories is working

---
+++ Craiglist/app/controllers/categories_controller.rb
@@ -0,0 +1,23 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def udpate
+  end
+
+  def delete
+  end
+end
